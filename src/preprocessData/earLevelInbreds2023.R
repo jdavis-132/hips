880b1a4840f11226377068a22265d76372d12b29
jdavis-132 <jdavis132@huskers.unl.edu>
2025-11-06 15:51:57 -0600

    wrangle 2023 inbred ear-level data

--- src/preprocessData/earLevelInbreds2023.R
+++ src/preprocessData/earLevelInbreds2023.R
@@ -57,7 +57,8 @@ for(file in cobFileList)
              str_replace('-25-', '125') %>% 
              str_replace('-22-', '-22'),
            stringCob = case_when(!is.na(stringCob) ~ 'Severe bend in ear. String used to measure cob length'), 
-           cobBroke = case_when(!is.na(cobBroke) ~ 'Cob broke in pieces during shelling')) %>%
+           cobBroke = case_when(!is.na(cobBroke) ~ 'Cob broke in pieces during shelling'), 
+           sampleID = case_when(sampleID=='23-C-1738109125' ~ '23-C-1738109-25', .default = sampleID)) %>%
     filter(str_detect(sampleID, 'A')|str_detect(sampleID, 'C')) %>%
     mutate(qrCode = str_split(sampleID, '-', simplify = TRUE)[1:3] %>% 
              str_flatten(collapse = '-')) 
@@ -128,7 +129,19 @@ ac_plotdata <- read_excel('rawData/2023/2023_yield_ICIA_v3.xlsx',
                           skip = 1) %>% 
   mutate(genotype = str_remove(genotype, '@'))
 
-ia_data <- left_join(ia_eardata, ac_plotdata, join_by(qrCode), relationship = 'many-to-one')
+ia_data <- left_join(ia_eardata, ac_plotdata, join_by(qrCode), relationship = 'many-to-one') %>% 
+  mutate(location = case_when(str_detect(qrCode, 'C') ~ 'Crawfordsville', 
+                              .default = 'Ames'), 
+         sublocation = location,
+         nitrogenTreatment = case_when(poundsOfNitrogenPerAcre==75 ~ 'Low', 
+                                       poundsOfNitrogenPerAcre==150 ~ 'Medium', 
+                                       poundsOfNitrogenPerAcre==225 ~ 'High'), 
+         block = case_when(nitrogenTreatment=='Medium' ~ rep + 2, 
+                           nitrogenTreatment=='High' ~ rep + 4, 
+                           .default = rep), 
+         plotNumber = str_split_i(qrCode, fixed('-'), 3) %>% 
+           as.numeric()) %>% 
+  select(!rep)
 
 # ne ear data
 ne_eardata <- read_csv('rawData/2023/inbreds/2023_Inbred_Hips_Ear_Phenotyping_MV_LNK_Final_KL_Curated_DataEntrySheet.csv', 
@@ -149,10 +162,45 @@ ne_eardata <- read_csv('rawData/2023/inbreds/2023_Inbred_Hips_Ear_Phenotyping_MV
          primaryKernelColor = stringr::str_remove_all(primaryKernelColor, fixed('$'))) %>% 
   unite('notes', c(rowbandNotes, earNotes, adminNotes), sep = ';', remove = TRUE, na.rm = TRUE) %>% 
   unite('kernelColor', c(primaryKernelColor, secondaryKernelColor), sep = ';', remove = TRUE, na.rm = TRUE) %>%
-  mutate(notes = case_when(notes=='' ~ NA, .default = notes))
-# ne metadata
-# join ne data
+  ungroup() %>%
+  mutate(notes = case_when(notes=='' ~ NA, .default = notes), 
+         location = str_split_i(qrCode, fixed('$'), 1) %>% 
+           str_to_title(), 
+         nitrogenTreatment = 'Medium', 
+         poundsOfNitrogenPerAcre = 150, 
+         sublocation = location, 
+         block = str_split_i(qrCode, fixed('$'), 6) %>% 
+           as.numeric(), 
+         plotNumber = str_split_i(qrCode, fixed('$'), 7) %>% 
+           str_split_i('ROW', 1) %>% 
+           str_remove('PLOT') %>% 
+           as.numeric(), 
+         row = str_split_i(qrCode, fixed('$'), 7) %>% 
+           str_split_i('ROW', 2) %>% 
+           as.numeric(), 
+         range = str_split_i(qrCode, fixed('$'), 8) %>% 
+           str_remove('RANGE') %>%
+           as.numeric(), 
+         genotype = str_split_i(qrCode, fixed('$'), 9) %>% 
+           str_remove('@'))
 # join ne & ia data
+inbreds <- bind_rows(ia_data, ne_eardata) %>% 
+  mutate(year = 2023, 
+         locationYear = str_c(year, location, sep = ' '), 
+         environment = str_c(locationYear, nitrogenTreatment, sep = ' '), 
+         genotype = str_to_upper(genotype)) %>% 
+  mutate(genotype = case_when(genotype=='AK-01' ~ 'UFMU-02421', 
+                              genotype=='AK-02' ~ 'UFMU-05595',
+                              genotype=='AK-03' ~ 'UFMU-07581',
+                              genotype=='AK-04' ~ 'UFMU-07868',
+                              genotype=='AK-05' ~ 'UFMU-08594',
+                              genotype=='AK-06' ~ 'UFMU-08805',
+                              genotype=='AK-07' ~ 'UFMU-08808',
+                              genotype=='AK-08' ~ 'UFMU-10402',
+                              genotype=='AK-09' ~ 'UFMU-11445',
+                              genotype=='AK-10' ~ 'W22 R1-R',
+                              genotype=='AK-11' ~ 'B73',
+                              .default = genotype))
 # check metadata
 # check correlation
 # write out 2023 ear level inbreds
